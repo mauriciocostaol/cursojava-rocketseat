96ee6c0555bf6685fccb8ba7f0f995a0ba0626b4
Mauricio Ricardo <mauriciod37@gmail.com>
2023-10-10 06:10:11 -0300

    Adicionado UserController

---
+++ src/main/java/br/com/mauricioricardo/todolist/user/UserController.java
@@ -0,0 +1,11 @@
+package br.com.mauricioricardo.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+
+}
