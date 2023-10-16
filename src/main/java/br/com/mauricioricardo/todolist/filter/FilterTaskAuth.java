84bf1410923a26cb3fd853d91ed9ed3cccc23ea7
Mauricio Ricardo <mauriciod37@gmail.com>
2023-10-15 23:15:56 -0300

    Finalização da API e preparação para deploy

--- src/main/java/br/com/mauricioricardo/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/mauricioricardo/todolist/filter/FilterTaskAuth.java
@@ -1,7 +1,5 @@
 package br.com.mauricioricardo.todolist.filter;
 
-
-
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.mauricioricardo.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
@@ -15,45 +13,42 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.Base64;
 
-
 @Component
-public class FilterTaskAuth  extends OncePerRequestFilter {
-@Autowired
-private IUserRepository userRepository;
+public class FilterTaskAuth extends OncePerRequestFilter {
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         var servletPath = request.getServletPath();
 
-        if(servletPath.startsWith("/tasks/")){
+        if (servletPath.startsWith("/tasks/")) {
             var authorization = request.getHeader("Authorization");
-
 
             var authEncoded = authorization.substring("Basic".length()).trim();
             byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
-            var authString = new  String(authDecoded);
+            var authString = new String(authDecoded);
 
             String[] credentials = authString.split(":");
             String username = credentials[0];
             String password = credentials[1];
 
             var user = this.userRepository.findByUsername(username);
-            if(user == null){
+            if (user == null) {
                 response.sendError(401);
-            }
-            else {
-                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(),user.getPassword());
-                if (passwordVerify.verified){
-                    request.setAttribute("idUser",user.getId());
-                    filterChain.doFilter(request,response);
-                }else {
+            } else {
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
                     response.sendError(401);
                 }
             }
+        } else {
+            filterChain.doFilter(request, response);
         }
-        else {
-            filterChain.doFilter(request,response);
-        }
-
 
     }
 }
--- src/main/java/br/com/mauricioricardo/todolist/task/TaskModel.java
+++ src/main/java/br/com/mauricioricardo/todolist/task/TaskModel.java
@@ -1,6 +1,5 @@
 package br.com.mauricioricardo.todolist.task;
 
-import br.com.mauricioricardo.todolist.user.UserModel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,21 +9,29 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+
 @Data
 @Entity(name = "tb_tasks")
 public class TaskModel {
-    @Id
-    @GeneratedValue(generator = "UUID")
-    private UUID id;
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
 
-    private String description;
+  private String description;
   @Column(length = 50)
-    private  String title;
-    private LocalDateTime startAt;
-    private LocalDateTime endAt;
-    private  String priority;
-    @CreationTimestamp
-    private  LocalDateTime createdAt;
+  private String title;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
-    private  UUID idUser;
+  private UUID idUser;
+
+  public void setTitle(String title) throws Exception{
+      if(title.length()>50){
+        throw new Exception("O campo title deve conter no máximo 50 caracteres");
+      }
+      this.title = title;
+    }
 }
